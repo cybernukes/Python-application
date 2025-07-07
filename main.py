2c5f63db59d536d209a8e4acd992bf82340b0cf5
cybernukes <115928309+cybernukes@users.noreply.github.com>
2025-07-07 11:14:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+num = int()
+
+@app.route('/')
+def index():
+    return render_template("webpage.html", num=num)
+
+@app.route('/hit', methods=['POST'])
+def hit():
+    data = request.get_json()
+    global num
+    num = data['Number']
+
+    return jsonify({"Message": "Data Recieved", "Data": num})
+
+if __name__ == "__main__":
+    app.run(debug=True)
