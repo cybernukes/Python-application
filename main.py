60893f38fac6b4de2f737263b171cac5d995cbe1
cybernukes <115928309+cybernukes@users.noreply.github.com>
2025-07-08 10:52:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,20 @@
 from flask import Flask, request, jsonify, render_template
-
 from flask_cors import CORS
 
 app = Flask(name)
-
 CORS(app)
-
 num = int()
 
 @app.route('/')
-
 def index():
-
-return render_template("webpage.html", num=num)
+    return render_template("webpage.html", num=num)
 
 @app.route('/hit', methods=['POST'])
-
 def hit():
-
-data = request.get_json()
-
-global num
-
-num = data['Number']
-
-
-
-return jsonify({"Message": "Data Recieved", "Data": num})
+    data = request.get_json()
+    global num
+    num = data['Number']
+    return jsonify({"Message": "Data Recieved", "Data": num})
 
 if name == "main":
-
-app.run(debug=True)
+    app.run(debug=True)
